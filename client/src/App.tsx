535b4c8fb06f633bdc382f876c9272b680a2c23f
dennyzain <abbasdenny24@gmail.com>
2022-04-18 11:18:15 +0700

    [client] added ant-design for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+import React from 'react';
+import './styles/App.css';
+import { Header } from 'antd/lib/layout/layout';
+import { Layout } from 'antd';
+
+const App:React.FC = () => (
+  <Layout>
+    <Header />
+  </Layout>
+);
 
 export default App;
---
+++ client/src/components/organisms/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header:React.FC = () => (
+  <div />
+);
+
+export default Header;
